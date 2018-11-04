c632c3bc6cf6e5162ba6d32b49c98973a8307cc7
liu <home_liu@qq.com>
2018-11-04 15:57:15 +0800

    自定义处理器 实现 ServeHTTP接口

--- server.go
+++ server.go
@@ -5,19 +5,38 @@ import (
 	"net/http"
 )
 
-func index(w http.ResponseWriter , r *http.Request){
-	fmt.Fprintf(w,"hello")
+//~~~~~~~~~~~基础服务
+// func index(w http.ResponseWriter , r *http.Request){
+// 	fmt.Fprintf(w,"hello")
+// }
+
+// func main(){
+	
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/",index)
+	
+// 	server :=http.Server{
+// 		Addr:":8080",
+// 		Handler:mux,
+// 	}
+// 	server.ListenAndServe()
+// 	fmt.Println("ok")
+// }
+
+
+//~~~~~~~~~~~自定义处理器
+type CustomHandler struct{}
+
+func (cus *CustomHandler) ServeHTTP(w http.ResponseWriter , r *http.Request){
+	fmt.Fprintf(w, "custome handle")
 }
 
 func main(){
-	
-	mux := http.NewServeMux()
-	mux.HandleFunc("/",index)
-	
-	server :=http.Server{
+	Custom := CustomHandler{}
+	server := http.Server{
 		Addr:":8080",
-		Handler:mux,
+		Handler: &Custom,
 	}
+
 	server.ListenAndServe()
-	fmt.Println("ok")
 }
